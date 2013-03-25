aa87ac6259376047f8db0c39b725ddb7e21a95e1
Arbo von Monkiewitsch <arbo@soundcloud.com>
2013-03-25 15:16:37 +0100

    Fix for multi-line tests

--- sheep.sh
+++ sheep.sh
@@ -1,9 +1,11 @@
 
+# http://github.com/arbovm/sheep
+
 SHEEP_INTERPRETER=${SHEEP_INTERPRETER-bash -c}
 
 function it() {
   local testdesc="$@"
-  local testbody="$(cat)"
+  local testbody="'$(cat)'"
   printf "$testdesc"
   run="set -x; eval '$testbody'"
   out="$($SHEEP_INTERPRETER "$run" 2>&1 )"
